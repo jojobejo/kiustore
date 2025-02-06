0069f8e08e036cd96edf433137b67d92f9dd1670
jojobejo <maulanamalik6696@gmail.com>
2025-02-06 08:51:29 +0700

    updated

--- application/modules/admin/controllers/Api_payment_briva.php
+++ application/modules/admin/controllers/Api_payment_briva.php
@@ -7,34 +7,281 @@ class api_payment_briva extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        // Muat library BRIVA yang telah dibuat
-        $this->load->helper('Briva_lib');
+
+        verify_session('admin');
+
+        $this->load->model(array(
+            'order_model' => 'order',
+            'payment_model' => 'payment'
+        ));
     }
 
-    /**
-     * Contoh fungsi untuk membuat Virtual Account
-     */
-    public function create_virtual_account()
+    public function index()
     {
-        // Data request yang sesuai dengan dokumentasi API BRIVA WS SNAP BI
-        $data = [
-            "partnerServiceId"   => "   77777", // Perhatikan padding jika diperlukan
-            "customerNo"         => "20098106",
-            "virtualAccountNo"   => "   7777720098106",
-            "virtualAccountName" => "Tes Surya",
-            "totalAmount"        => [
-                "value"    => "10000.00",
-                "currency" => "IDR"
-            ],
-            "expiredDate"        => "2024-02-28T22:38:25+07:00",
-            "trxId"              => "abcdefgh1234",
-            "additionalInfo"     => [
-                "description" => "Keterangan pembuatan VA"
-            ]
-        ];
+        $params['title'] = 'Payment VIA BRIVA-API';
+        $data['briva']   = $this->payment->payment_bri();
 
-        $result = $this->briva_lib->create_va($data);
-        header('Content-Type: application/json');
-        echo json_encode($result);
+        $this->load->view('header', $params);
+        $this->load->view('payments/brivapayments', $data);
+        $this->load->view('footer');
     }
+
+    // <?php
+    // date_default_timezone_set("Asia/Jakarta");
+    
+    // $url = 'https://sandbox.partner.api.bri.co.id';
+    // $privateKey = file_get_contents('private_key_rsa.pem');
+    // $client_secret = 'HYhwDH5dgBqisXJ7';//Consumer Secret
+    // $client_id = '8vXZAhvethvJvKa1rmNDbH9ZxlX2Xmen';//Consumer Key
+    // $xPartnerId = 'kuionline'; // di generate oleh bri
+    // $partnerServiceId = '   22001'; // di generate oleh bri
+    // $customerNo = '00218322'; // di generate oleh partner                                                   
+    
+    // #getToken();die;
+    // #createVa();die;
+    // #updateVa();
+    // inquiryVa();die;
+    // #updateStatusVa();die;
+    // #inquiryStatusVa();die;
+    // #deleteVa();die;
+    
+    // function inquiryStatusVa(){
+    //     global $url,$partnerServiceId,$customerNo;
+        
+    //     $patch = '/snap/v1.0/transfer-va/status';
+    //     $fullUrl = $url.$patch;
+    //     $method = 'POST';
+    //     $timestamp = date('c');
+    //     $token = getToken();
+        
+    //     $body = array(
+    //         'partnerServiceId'  => $partnerServiceId,
+    //         'customerNo'        => $customerNo,
+    //         'virtualAccountNo'  => $partnerServiceId.$customerNo,
+    //         'inquiryRequestId'  => 'trx12345'
+    //     );
+        
+    //     curlEndpoint($fullUrl,$token,$timestamp,$method,$patch,$body,'-- inquiry status va --');
+    // }
+    
+    // function deleteVa(){
+    //     global $url,$partnerServiceId,$customerNo;
+        
+    //     $patch = '/snap/v1.0/transfer-va/delete-va';
+    //     $fullUrl = $url.$patch;
+    //     $method = 'DELETE';
+    //     $timestamp = date('c');
+    //     $token = getToken();
+        
+        
+    //     $body = array(
+    //         'partnerServiceId'  => $partnerServiceId,
+    //         'customerNo'        => $customerNo,
+    //         'virtualAccountNo'  => $partnerServiceId.$customerNo,
+    //     );
+        
+    //     curlEndpoint($fullUrl,$token,$timestamp,$method,$patch,$body,'-- delete va --');
+    // }
+    
+    // function inquiryVa(){
+    //     global $url,$partnerServiceId,$customerNo;
+        
+    //     $patch = '/snap/v1.0/transfer-va/inquiry-va';
+    //     $fullUrl = $url.$patch;
+    //     $method = 'POST';
+    //     $timestamp = date('c');
+    //     $token = getToken();
+        
+        
+    //     $body = array(
+    //         'partnerServiceId'  => $partnerServiceId,
+    //         'customerNo'        => $customerNo,
+    //         'virtualAccountNo'  => $partnerServiceId.$customerNo,
+    //         'trxId'             => 'trx12346', // di generate oleh partner 
+    //     );
+        
+    //     curlEndpoint($fullUrl,$token,$timestamp,$method,$patch,$body,'-- inquiry va --');
+    // }
+    
+    // function updateStatusVa(){
+    //     global $url,$partnerServiceId,$customerNo;
+        
+    //     $patch = '/snap/v1.0/transfer-va/update-status';
+    //     $fullUrl = $url.$patch;
+    //     $method = 'PUT';
+    //     $timestamp = date('c');
+    //     $token = getToken();
+        
+    //     $body = array(
+    //         'partnerServiceId'  => $partnerServiceId,
+    //         'customerNo'        => $customerNo,
+    //         'virtualAccountNo'  => $partnerServiceId.$customerNo,
+    //         'trxId'             => 'trx12346', // di generate oleh partner 
+    //         'paidStatus'        => 'Y',
+    //     );
+        
+    //     curlEndpoint($fullUrl,$token,$timestamp,$method,$patch,$body,'-- update status va --');
+    // }
+    
+    // function updateVa(){
+    //     global $url,$partnerServiceId,$customerNo;
+        
+    //     $patch = '/snap/v1.0/transfer-va/update-va';
+    //     $fullUrl = $url.$patch;
+    //     $method = 'PUT';
+    //     $timestamp = date('c');
+    //     $token = getToken();
+        
+    //     $body = array(
+    //         'partnerServiceId'  => $partnerServiceId,
+    //         'customerNo'        => $customerNo,
+    //         'virtualAccountNo'  => $partnerServiceId.$customerNo,
+    //         'virtualAccountName'=> 'tes va ite malang',
+    //         'trxId'             => 'trx12346', // di generate oleh partner 
+    //         'totalAmount'       => array(
+    //                                 'value'     => '20000.00',
+    //                                 'currency'  => 'IDR'
+    //                             ),
+    //         'expiredDate'       => date('c',strtotime('2024-06-30 23:00')),
+    //         'additionalInfo'    => array(
+    //                                 'description'   => 'keterangan'
+    //                             )    
+    //     );
+    //     #echoPre($body);die;
+    //     curlEndpoint($fullUrl,$token,$timestamp,$method,$patch,$body,'-- update va --');
+    // }
+    
+    // function createVa(){
+    //     global $url,$partnerServiceId,$customerNo;
+        
+    //     $patch = '/snap/v1.0/transfer-va/create-va';
+    //     $fullUrl = $url.$patch;
+    //     $method = 'POST';
+    //     $timestamp = date('c');
+    //     $token = getToken();
+    
+    //     $body = array(
+    //         'partnerServiceId'  => $partnerServiceId,
+    //         'customerNo'        => $customerNo,
+    //         'virtualAccountNo'  => $partnerServiceId.$customerNo,
+    //         'virtualAccountName'=> 'tes lagi ite malang',
+    //         'totalAmount'       => array(
+    //                                 'value'     => '11000.00',
+    //                                 'currency'  => 'IDR'
+    //                             ),
+    //         'expiredDate'       => date('c',strtotime('2024-06-30 23:00')),
+    //         'trxId'             => 'trx12346', // di generate oleh partner 
+    //         'additionalInfo'    => array(
+    //                                 'description'   => 'keterangan'
+    //                             )    
+    //     );
+
+    //     curlEndpoint($fullUrl,$token,$timestamp,$method,$patch,$body,'-- inquiry va --');
+    // }
+    
+    // function getToken(){
+    //     global $url,$client_id;
+    //     $patch = '/snap/v1.0/access-token/b2b';
+    //     $fullUrl = $url.$patch;
+    //     $timestamp = date('c');
+        
+        
+    //     $headers = array(
+    //         'X-SIGNATURE:'.asymmetricSignature($client_id,$timestamp),
+    //         'X-CLIENT-KEY:'.$client_id,
+    //         'X-TIMESTAMP:'.$timestamp,
+    //         'Content-Type:application/json',
+    //     );
+
+    //     $body = array(
+    //         'grantType' => 'client_credentials'
+    //     );
+        
+    //     $ch = curl_init();
+    //     curl_setopt($ch, CURLOPT_URL,$fullUrl);
+    //     curl_setopt($ch, CURLOPT_CUSTOMREQUEST,'POST');
+    //     curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($body));
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    //     #curl_setopt($ch, CURLOPT_HEADER,true);
+    //     $response = curl_exec($ch);
+    //     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    //     curl_close($ch);
+    //     $token = json_decode($response,true);
+
+    //     echoPre('-- create token --');
+    //     echoPre($response);
+
+    //     return $token['accessToken'];
+    // }
+    
+    // function curlEndpoint($fullUrl,$token,$timestamp,$method,$patch,$body,$remark){
+    //     global $xPartnerId;
+        
+    //     $headers = array(
+    //         'Authorization:Bearer '.$token,
+    //         'X-TIMESTAMP:'.$timestamp,
+    //         'X-SIGNATURE:'.symmetricSignature($method,$patch,$body,$timestamp,$token),
+    //         'Content-Type:application/json',
+    //         'X-PARTNER-ID:'.$xPartnerId,
+    //         'CHANNEL-ID:00001',
+    //         'X-EXTERNAL-ID:'.rand(100000000,999999999) // di generate oleh partner , random setiap hari
+    //     );
+
+    //     $ch = curl_init();
+    //     curl_setopt($ch, CURLOPT_URL,$fullUrl);
+    //     curl_setopt($ch, CURLOPT_CUSTOMREQUEST,$method);
+    //     curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($body));
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    //     #curl_setopt($ch, CURLOPT_HEADER,true);
+    //     $response = curl_exec($ch);
+    //     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    //     curl_close($ch);
+
+    //     echoPre($remark);
+    //     echoPre(json_decode($response,true));
+    // }
+    
+    // function symmetricSignature($method,$path,$body,$timestamp,$accessToken){
+    //     global $client_secret;//Consumer Secret
+        
+    //     $hashBody = json_encode($body);// Body minify
+    //     $hashBody = hash('sha256', $hashBody);// Calculate Hash with sha256
+    //     $signedBody = strtolower($hashBody);// Convert to lowercase
+        
+    //     $stringToSign = implode(':', [
+    //         $method,
+    //         $path,
+    //         $accessToken,
+    //         $signedBody,
+    //         $timestamp
+    //     ]);
+        
+    //     $signature = hash_hmac('sha512', $stringToSign, $client_secret, true);
+        
+    //     // X-SIGNATURE
+    //     return base64_encode($signature);
+    // }
+    
+    // function asymmetricSignature($client_id,$timestamp){
+    //     global $privateKey;
+        
+    //     $stringToSign = $client_id.'|'.$timestamp;
+    //     $signature = "";
+    //     if (!openssl_sign($stringToSign, $signature, $privateKey, OPENSSL_ALGO_SHA256)) {
+    //         throw new SignatureException("Failed to generate signature");
+    //     }
+
+    //     // X-SIGNATURE
+    //     return base64_encode($signature);
+    // }
+    
+    // function echoPre($var){
+    //     echo '<pre>';
+    //     print_r($var);
+    //     echo '</pre>';
+    // }
+
+
 }
--- application/modules/customer/views/shop/cart.php
+++ application/modules/customer/views/shop/cart.php
@@ -125,7 +125,34 @@ defined('BASEPATH') or exit('No direct script access allowed');
             </div>
           <?php endforeach; ?>
         <?php endif; ?>
-        <form action="<?= base_url('checkout'); ?>" method="POST">
+        <div hidden>
+          <form action="<?= base_url('checkout'); ?>" method="POST">
+            <input type="text" name="customer" value="<?= $this->session->userdata('user_id') ?>">
+            <input id="code" name="coupon_code" type="text" class="form-control" placeholder="">
+            <?php foreach ($carts as $item) : ?>
+              <div class="swipe-to-show cart-<?php echo $item['rowid']; ?>">
+                <div class="product-list media">
+                  <a href="#"><img src="<?php echo get_product_image($item['id']); ?>" alt="offer" /></a>
+                  <div class="media-body">
+                    <a href="#" class="font-sm"> <?php echo $item['name']; ?> </a>
+                    <span class="content-color font-xs">Rp <?php echo format_rupiah($item['price']); ?> x <span class="qty-item-<?php echo $item['rowid']; ?>"><?php echo $item['qty']; ?> <?php echo $item['satuan_text']; ?></span></span>
+                    <span class="title-color subtotal-item-<?php echo $item['rowid']; ?> font-sm">Rp <?php echo format_rupiah($item['subtotal']); ?></span>
+                    <div class="plus-minus">
+                      <i class="subs" data-feather="minus"></i>
+                      <input class="cart-update" name="quantity[<?php echo $item['rowid']; ?>]" type="number" data-qty="<?php echo $item['qty']; ?>" data-rowid="<?php echo $item['rowid']; ?>" value="<?php echo $item['qty']; ?>" min="0" max="1000" />
+                      <i class="adds" data-feather="plus"></i>
+                    </div>
+                  </div>
+                </div>
+                <div class="delete-button" data-bs-toggle="offcanvas" data-bs-target="#confirmation" aria-controls="confirmation" data-rowid="<?php echo $item['rowid']; ?>">
+                  <i data-feather="trash"></i>
+                </div>
+              </div>
+            <?php endforeach; ?>
+        </div>
+        <button type="submit" class="btn btn-success" style="width: 98%; margin-bottom: 10px; margin-left: 12px;">checkout</button>
+        </form>
+        <!-- <form action="<?= base_url('checkout'); ?>" method="POST">
           <input type="text" name="customer" value="<?= $this->session->userdata('user_id') ?>">
           <input id="code" name="coupon_code" type="text" class="form-control" placeholder="">
           <?php foreach ($carts as $item) : ?>
@@ -149,7 +176,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
             </div>
           <?php endforeach; ?>
           <button type="submit" class="btn btn-success" style="width: 98%; margin-bottom: 10px; margin-left: 12px;">checkout</button>
-        </form>
+        </form> -->
       <?php endforeach; ?>
 
     </main>
