16f03cb13198c3ea2cdea6020728a8495e34a62a
IT Karisma <it.karisma89@gmail.com>
2026-07-19 20:21:47 +0700

    updated

--- application/core/MY_Router.php
+++ application/core/MY_Router.php
@@ -3,4 +3,14 @@
 /* load the MX_Router class */
 require APPPATH."third_party/MX/Router.php";
 
-class MY_Router extends MX_Router {}
+class MY_Router extends MX_Router {
+	/**
+	 * CI_URI class object.
+	 *
+	 * Declared for PHP 8.2+ compatibility; CI_Router assigns this property
+	 * during construction.
+	 *
+	 * @var object
+	 */
+	public $uri;
+}
---
+++ application/core/MY_URI.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_URI extends CI_URI {
+	/**
+	 * CI_Config class object
+	 *
+	 * Declared for PHP 8.2+ compatibility; CI_URI assigns this property
+	 * during construction.
+	 *
+	 * @var object
+	 */
+	public $config;
+}
